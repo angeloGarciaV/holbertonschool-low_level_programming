10674314c4dd038565f448617673423615b00b0b
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-16 11:25:11 -0400

    Fix for 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point
- *
- * Description: Prints a message using write()
- *
- * Return: 0 (Success)
+ * main - entry point
+ * Return: always 0 (success)
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
