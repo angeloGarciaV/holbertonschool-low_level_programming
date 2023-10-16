8137564847b8473425d8a5993e94bda421ceec18
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-16 10:50:07 -0400

    Fix betty formatting for 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/syscall.h>
 
 /**
  * main - prints a message using syscall
- *
  * Return: -1 (errno)
  */
 
-int main(void) 
-{ 
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-    syscall(SYS_write, 1, msg, sizeof(msg)); 
-    return (-1);
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	syscall(SYS_write, 1, msg, sizeof(msg));
+	return (-1);
 }
