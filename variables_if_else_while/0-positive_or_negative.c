b70b62b7f1db65a3708cc244eea0e50abfe79a71
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-16 13:44:58 -0400

    Created 0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - determines if 'n' is positive or negative
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive");
+	} else if (n < 0)
+	{
+		printf("is negative");
+	} else{
+		printf("is zero");
+	}
+	}
+	return (0);
+
+}
