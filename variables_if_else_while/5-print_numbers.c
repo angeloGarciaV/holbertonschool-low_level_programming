1d4e38735595b31a7442dfd9763c9c5eb78ae5c7
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-17 19:51:22 -0400

    Created 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - For loop that prints base 10 numbers(0-9)
+  *
+  * Return: 0 (success)
+  *
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
