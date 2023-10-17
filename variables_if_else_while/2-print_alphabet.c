e216a6fdd81285e0ee0bd141508887855a32082e
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-17 12:10:07 -0400

    Fix for 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-
-int main ()
+/**
+ * main - Uses putchar to evaluate characters as their binary representations.
+ * The following for loop iterates through characters a-z and prints them.
+ * the second putchar adds a new line after the results of the for loop.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
 {
 	char c;
-   
-   for(c = 'a' ; c <= 'z' ; c++) {
-	   putchar(c);
-   }
-   putchar('\n');
-   return(0);
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
 }
