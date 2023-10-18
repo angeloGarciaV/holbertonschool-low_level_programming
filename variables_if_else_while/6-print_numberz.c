fc81b09a5d580965be478bdda5ce01cd5f97ecab
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-18 08:59:22 -0400

    Created 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 /**
+  * main - uses ASCII numbers to print 0-9 with putchar()
   *
-  *
-  *
+  * Return: 0 (success)
   */
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+int main(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
