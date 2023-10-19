a5fe96f2c92a196415be45adeb5ee87401cb22cd
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-19 10:13:04 -0400

    Created 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - uses print_alphabet prototype to call the print_alphabet function
+  *
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+  * print_alphabet - uses a for loop to print the alphabet in lowercase
+  *
+  * Return: void (Success)
+  */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+}
