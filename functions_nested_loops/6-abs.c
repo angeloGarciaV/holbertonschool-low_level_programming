2ac4e49c6a2c4002411e3d6760c701c704093439
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-20 19:28:51 -0400

    Created 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+  * _abs- prints the absolute value of an interger
+  * @n:the number to check
+  *
+  * Return: 0 (Success)
+  */
+int _abs(int n)
+{
+	int i = abs(n);
+
+	return (i);
+}
