3afcebe86b4052cf1ec40a458ca30ae7f8b161bf
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-21 18:03:20 -0400

    Created directorty, README.md, and 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.c>
+#include <ctype.h>
+#include "main.h"
+
+/**
+  * _isupper - if statement 
+  * @c:
+  * Return:
+  */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
+
