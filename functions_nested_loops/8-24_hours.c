5daebad74b1f41d7ffbcaded0babb2e3169f14e7
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-20 23:08:19 -0400

    Fix for 8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 {
 	int hr1, hr2, min1, min2;
 
-	for (hr1 = 0; hr1 <= 2; hr1++)
+	for (hr1 = 0; hr1 < 2; hr1++)
 	{
-		for (hr2 = 0; hr2 < 4; hr2++)
+		for (hr2 = 0; hr2 <= 9; hr2++)
 		{
 			for (min1 = 0; min1 <= 5; min1++)
 			{
@@ -22,4 +22,22 @@ void jack_bauer(void)
 			}
 		}
 	}
+	for (hr1 = 2; hr1 <= 2; hr1++)
+	{
+		for (hr2 = 0; hr2 <= 3; hr2++)
+		{
+			for (min1 = 0; min1 <= 5; min1++)
+			{
+				for (min2 = 0; min2 <= 9; min2++)
+				{
+					printf("%d%d:%d%d\n", hr1, hr2, min1, min2);
+				}
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	jack_bauer();
 }
