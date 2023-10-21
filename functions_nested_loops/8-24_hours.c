b615315fc93774730fa644de1e0260c577ad8bda
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-20 22:13:37 -0400

    Created 8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * jack_bauer - uses a for loops to print hours and minutes.
+  *
+  * Return: 0
+  */
+void jack_bauer(void)
+{
+	int hr1, hr2, min1, min2;
+
+	for (hr1 = 0; hr1 <= 2; hr1++)
+	{
+		for (hr2 = 0; hr2 < 4; hr2++)
+		{
+			for (min1 = 0; min1 <= 5; min1++)
+			{
+				for (min2 = 0; min2 <= 9; min2++)
+				{
+					printf("%d%d:%d%d", hr1, hr2, min1, min2);
+				}
+			}
+		}
+	}
+}
