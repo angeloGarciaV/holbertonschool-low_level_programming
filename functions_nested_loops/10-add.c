bbc95dd3bf98ad0d199148377d742f85af7bf74c
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-21 12:27:13 -0400

    Created 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * add - adds two integers
+  * @a: first integer to be added
+  * @b: second integer to be added
+  * Return: returns result of a+b
+  */
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
