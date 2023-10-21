e4c8e5782387cc97b6029bbbdc76457fdbe90215
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-21 19:13:17 -0400

    Created 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * mul - multiplies a and b
+  * @a: first parameter
+  * @b: second parameter
+  * Return: result of a * b
+  *
+  */
+int mul(int a, int b)
+{
+	int i = a * b;
+
+	return (i);
+}
