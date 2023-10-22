7d6de27027732f6a221f1d2766c8aead7c754f5c
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-21 21:43:17 -0400

    Created 7-print_diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * print_diagonal - prints a diagonal line on the terminal
+  * @n: amount of spaces before printing a slash
+  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+}
