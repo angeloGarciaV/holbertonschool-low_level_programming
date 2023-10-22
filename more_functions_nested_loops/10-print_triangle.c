adc99ec5815976af6652590601bf161c0d039da2
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-21 23:43:11 -0400

    Created 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * print_triangle - uses a for loop to print a triangle made of number signs
+  * @size: determines the size of the triangle
+  */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j <= size; j++)
+		{
+			if(j < (size - i))
+			{
+			putchar(' ');
+			}
+			else
+			{
+				putchar('#');
+			}
+		}
+		putchar('\n');
+	}
+}
