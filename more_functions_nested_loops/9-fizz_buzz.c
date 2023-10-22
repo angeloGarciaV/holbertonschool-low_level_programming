93492ad8538a86fcc4dc3ff0d089097aad78151c
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-21 22:46:36 -0400

    Fix for 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
-  * main -
+  * main - uses a for loop to count from 1 -100.
+  * The if statement checks whether a number is divisible by 3 or 5 and prints
+  * 'Fizz' or 'Buzz' respectively. If the number is divisible by both,
+  * it prints 'FizBuzz'.
   *
   * Return: 0 (Success)
   */
