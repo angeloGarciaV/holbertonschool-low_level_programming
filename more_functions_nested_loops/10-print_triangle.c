e4eeeecc045687f23f85044949864da84557e9fb
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-21 23:49:28 -0400

    Fix for 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,12 +11,13 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		putchar('\n');
+		return;
 	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= size; j++)
+		for (j = 0; j < size; j++)
 		{
-			if(j < (size - i))
+			if (j < size - i - 1)
 			{
 			putchar(' ');
 			}
