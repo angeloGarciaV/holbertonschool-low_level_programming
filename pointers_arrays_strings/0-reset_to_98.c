39a58c70117cb873972c3004a0710fbb5a9ec039
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-24 09:23:02 -0400

    Created pointers_arrays_strings directory, README.md, and 0-reset_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * reset_to_98 - Function that takes a pointer to an int as a parameter
+  * and updates the value it points to to 98.
+  *
+  * @n: parameter to be reset
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
