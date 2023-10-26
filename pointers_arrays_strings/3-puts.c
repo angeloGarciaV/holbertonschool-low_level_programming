a18b2bc1b0a19e56ab5b77ff5bda51ec57439480
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-26 10:06:55 -0400

    Created 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _puts -function that prints a stringm followed by a new line to stdout.
+  * @str:String to print.
+  */
+void _puts(char *str)
+{
+	puts(str);
+}
