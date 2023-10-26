540e99370bda1d902f606a471b38da75f11e3282
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-26 11:27:44 -0400

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <string.h>
 /**
@@ -9,7 +10,7 @@ void print_rev(char *str)
 	int i;
 	int ing = strlen(str);
 
-	for (i = ing; i >= 0; i--)
+	for (i = ing - 1; i >= 0; i--)
 	{
 		_putchar(str[i]);
 	}
