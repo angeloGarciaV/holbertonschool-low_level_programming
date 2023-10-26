603c37fd3356d253e75269b60544198e99b00b38
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-26 11:11:28 -0400

    Created 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * print_rev -function that prints a string in reverse.
+  * @str:String to print.
+  */
+void print_rev(char *str)
+{
+	int i;
+	int ing = strlen(str);
+
+	for (i = ing; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
