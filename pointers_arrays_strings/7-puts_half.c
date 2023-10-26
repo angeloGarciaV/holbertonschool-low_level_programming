a1dd4b13bc60d7fbe0fd1df92824609df99ec79e
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-26 18:04:45 -0400

    Created 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * puts_half - Function that prints half of a string.
+  * @str: String to be halved
+  */
+void puts_half(char *str)
+{
+	int i;
+	int j = strlen(str);
+	int k = strlen(str) / 2;
+
+	for (i = k; i < j; i++)
+	{
+		putchar(str[i]);
+	}
+}
