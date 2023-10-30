1ba9bc9bc3f0a8f9a3eaecd57cf2d069ef0658f4
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-30 12:56:59 -0400

    Fix for 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,17 @@
   */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j;
+	int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				return (s + 1);
-			j++;
+			{
+				return (s + i);
+			}
 		}
-		i++;
 	}
 	return (NULL);
 }
