bba810ebf71957ef51a01104ff005ae7fc089877
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-29 20:21:09 -0400

    Created 6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include <stdio.c>
+/**
+  * cap_string - Function that capitalizes all words of a string
+  * @s: string to capitalize
+  * Return: Capitalized string
+  */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char spaces[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+			 '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; spaces[j] != '\0'; j++)
+		{
+			if (i == 0)
+			{
+				if (s[i] >= 'a' && s[i] <= 'z')
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+			if (s[i] == spaces[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
