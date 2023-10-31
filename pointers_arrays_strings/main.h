769d27b39c20cbaeeeeb531c9727e98d6a3d23ea
Angelo Garcia <angelogarcia1029@gmail.com>
2023-10-30 23:08:05 -0400

    Created 5-strstr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,3 +19,4 @@ char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
