099410f66495f3d13395cd82cb41d249ad39393f
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-03 14:52:26 -0400

    is it working now?

---
+++ variables_if_else_while/100-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Uses a for loop to iterate through base 16 digits and converts them
+ * to ASCII so they be printed with putchar().
+ * the if statements place the comas and spaces.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n,o;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar (n + '0');
+
+		if (n < 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+		putchar ('\n');
+		return (0);
+}
