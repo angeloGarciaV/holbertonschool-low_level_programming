34b4fa756b232fcb1167f0bcc97fe94580421157
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-04 17:55:32 -0400

    Created argc_argv directory, README.md, and 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - Program that prints its name
+  * Return: 0
+  * @argc:count
+  * @argv: array
+  */
+int main(int argc, char argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
