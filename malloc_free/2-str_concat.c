a4c38043a89723da37cdf36ed5c8e6f7ab5931a3
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-12 16:04:22 -0400

    Fix for 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,10 +19,15 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{}
-	for (j = 0; s2[j] != '\0'; j++)
-	{}
+
+	while (s1[i])
+	{
+		i++;
+	}
+	while (s2[j])
+	{
+		j++;
+	}
 
 	k = i + j;
 
