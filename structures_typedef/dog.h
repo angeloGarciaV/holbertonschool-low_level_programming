bec52255885725e4268d5c34b1373fa0bd0dc81d
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-14 01:38:16 -0400

    Fix for dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,17 @@
-#pragma once
+#ifndef dog
+#define dog
 #include <stdio.h>
 #include <string.h>
-
+/**
+  * struct dog - dog structure
+  * @name: string for name
+  * @age: int for age
+  * @owner: string for name
+  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
