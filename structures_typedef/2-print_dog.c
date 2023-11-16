9b318663bb08dc7f741113926279350915d73587
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-16 19:03:35 -0400

    Fix for 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,30 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 /**
-  * print_dog - Function that prints a struct dog
-  * @d: Struct to print
-  */
+ * print_dog - Function that prints a struct dog
+ * @d: Struct to print
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		exit(0);
-	}
-	if (d->name == NULL)
-	{
-		printf("Name: (nil)");
-	}
-	if (d->age < 0)
-	{
-		printf("Age: (nil)");
-	}
-	if (d->owner == NULL)
-	{
-		printf("Owner: (nil)")
+		printf("(nil)\n");
+		return;
 	}
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %.1f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Name: ");
+	if (d->name == NULL)
+	{
+		printf("(nil)\n");
+	} else
+	{
+		printf("%s\n", d->name);
+	}
+
+	printf("Age: ");
+	if (isnan(d->age) || d->age < 0)
+	{
+		printf("(nil)\n");
+	} else
+	{
+		printf("%.1f\n", d->age);
+	}
+	
+	printf("Owner: ");
+	if (d->owner == NULL)
+	{
+		printf("(nil)\n");
+	} else
+	{
+		printf("%s\n", d->owner);
+	}
 }
