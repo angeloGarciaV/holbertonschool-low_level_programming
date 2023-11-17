38f94f58abc44570fdeefb672d78ea7735fdd90f
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-17 12:19:47 -0400

    Modified dog.h for Structures, typedef Task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,6 +15,6 @@ typedef struct
 	char *owner;
 }dog_t;
 
-void init_dog(struct dog_t *d, char *name, float age, char *owner);
-void print_dog(struct dog_t *d);
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 #endif
