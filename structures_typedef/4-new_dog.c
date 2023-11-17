6cd289cb18d0630de247765613c5489f2b6972a2
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-17 13:02:31 -0400

    Created 4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <string,h>
+/**
+  * new_dog - function that creates a new dog
+  * @name: array for name
+  * @age: float for age
+  * @owner: array for owner
+  * Return: new dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *arr;
+
+	arr = malloc(sizeof(dog_t));
+	if (arr == NULL)
+		return (NULL);
+
+	(*arr).name = malloc(sizeof(name) + 1);
+	if (name == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	(*arr).owner = malloc(sizeof(owner) + 1);
+	if ((*arr).owner == NULL)
+	{
+		free((*arr).name);
+		free(arr);
+		return (NULL);
+	}
+
+	strcpy((*arr).name, name);
+	strcpy((*arr).owner, owner);
+	(*arr).age = age;
+
+	return (arr);
+}
