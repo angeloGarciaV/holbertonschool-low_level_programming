3ea253e788710943991ddfada1719ea664cf5dfa
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-19 05:52:49 -0400

    Fix for 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,12 +8,12 @@
   */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	int x;
+
 	va_list args;
 
 	va_start(args, n);
-
-	unsigned int i;
-	int x;
 
 	for (i = 0; i < n; i++)
 	{
