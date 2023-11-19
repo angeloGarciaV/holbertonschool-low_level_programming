b9557f4a3736ab1e250417f991ce3d9728ac0347
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-19 05:12:18 -0400

    Created Variadic Functions directory, README.md, variadic_functions.h, and 0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+  * sum_them_all - Function that returns the sum of all parameter
+  * @n: number of arguments
+  * Return: sum of all numbers or 0 if no parameters
+  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, x, y;
+
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	y = 0;
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		y += x;
+	}
+	printf("%d\n", y);
+
+	va_end(args);
+	return (0);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef _VARIADIC_
+#define _VARIADIC_
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
