91cf73dbf1e4cbbc0908531ba8bdb6c8a93ca915
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-18 20:05:24 -0400

    Created 1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+  * array_iterator - array to iterate
+  * @array: array of integers
+  * @size:size of array
+  * @action: pointer to function
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
