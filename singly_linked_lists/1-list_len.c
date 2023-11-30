a1b1f77c351007d79a25f0a4bd429c1f7f4a941a
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-30 11:53:34 -0400

    Betty formatting 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -7,12 +7,12 @@
 */
 size_t list_len(const list_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h != NULL)
-    {
-        len++;
-        h = h->next;
-    }
-    return (len);
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
