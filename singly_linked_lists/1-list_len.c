0c3affd0f58e08a46637847352b79b6bcb6e3372
Angelo Garcia <angelogarcia1029@gmail.com>
2023-11-30 11:46:02 -0400

    Created 1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - function that returns the number of elements in a linked list
+ * @h:list_t struct
+ * Return: number of nodes
+*/
+size_t list_len(const list_t *h)
+{
+    size_t len = 0;
+
+    while (h != NULL)
+    {
+        len++;
+        h = h->next;
+    }   
+}
