c8d99f82aad8bb4946b60ac831ab8cbc14231be0
Angelo Garcia <angelogarcia1029@gmail.com>
2023-12-01 02:08:08 -0400

    Fix for 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,19 +13,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode = malloc(sizeof(list_t));
-
-	if ((*head) == NULL)
-	{
-		return (NULL);
-	}
+	
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
 
-	newNode->next = (*head)->next;
-	(*head)->next = newNode;
 	newNode->str = strdup(str);
+	newNode->next = *head;
+	*head = newNode;
 
-	return (newNode);
+	return newNode;
 }
