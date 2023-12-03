9da4d8db9bf165b4ddf213f793dcc75ba33d88a3
Angelo Garcia <angelogarcia1029@gmail.com>
2023-12-02 23:15:59 -0400

    Created Bit Manipulation directory, README.md, and 0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,31 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * Return: converted number or 0 if b is NULL or contains chars not 0 or 1
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: pointer to a string of 0 and 1 chars
+* Return: converted number or 0 if b is NULL or contains chars not 0 or 1
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, j, len, sum = 0;
+	unsigned int result = 0;
 
 	if (b == NULL)
-		return (0);
-
-	len = strlen(b);
-
-	for (i = 0, j = len - 1; i < len; i++, j--)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-
-		if (b[i] == '1')
-			sum += pow(2, j);
+		return (0);
 	}
 
-	return (sum);
+	while (*b)
+	{
+		if (*b == '0' || *b == '1')
+		{
+			result = (result << 1) + (*b - '0');
+			b++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result);
 }
